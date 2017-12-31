f6956613e27316ab35d4fb678cef631d546d7a09
tofu511 <mokmok511@gmail.com>
2017-12-31 19:31:47 +0900

    add DI setting

---
+++ app/AppModule.scala
@@ -0,0 +1,9 @@
+import com.google.inject.AbstractModule
+import services.{TaskService, TaskServiceImpl}
+
+class AppModule extends AbstractModule {
+
+  override def configure(): Unit = {
+    bind(classOf[TaskService]).to(classOf[TaskServiceImpl])
+  }
+}
